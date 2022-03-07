34bd990b3a0e6ffbe6fe93112fd450d546b536ba
dana26kw <dana1a2b3c@gmail.com>
2022-03-07 18:51:20 +0300

    i made a small description about mario

---
+++ c2-cw1 dana/c2-cw1 dana/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c2_cw1_dana { 
+    class Program
+{
+    static void Main(string[] args)
+    { string charctername = "mario";
+        string superpower1 = "flying";
+        string superpower2 = "burning";
+        int age = 15;
+        double charcterspeed = 66.6;
+
+        Console.WriteLine("my name is {0},my superpower1 {1},my superpower2 {2},my age is {3},my speed is s{4}",charctername,superpower1,superpower2,age,charcterspeed);
+    }
+}
+}
